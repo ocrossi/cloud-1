0911b4e4aacb471c76844d50bb0630ac492c01c6
Ubuntu <ubuntu@ip-172-31-11-0.eu-west-3.compute.internal>
2024-07-31 15:17:43 +0000

    add start of site on reboot

--- commands.sh
+++ commands.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update -yq && sudo apt-get upgrade -yq && sudo apt-get install pipx
-pipx install --include-deps ansible
+sudo apt-get update -yq && sudo apt-get upgrade -yq && sudo apt-get install ansible
