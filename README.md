2b6e84f9a471002fb2f62254a6c410c23e8e9574
ocrossi <zhdoctor971@gmail.com>
2024-09-05 17:16:52 +0200

    final version

--- README.md
+++
@@ -1,8 +0,0 @@
-what we use
-Ansible to install docker & docker compose
-
-TODO
-add ssl
-
-WHEN READY FOR CORR
-remove .gitignore ffile for wp persistency
--- commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-apt-get update -yq && apt-get upgrade -yq && apt-get -yq install ansible
-
-ansible-playbook -i localhost single_playbook.yml -e ansible_user="ansible_user"
-
